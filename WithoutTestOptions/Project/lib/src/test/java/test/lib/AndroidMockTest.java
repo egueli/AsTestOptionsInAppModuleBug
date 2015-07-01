5d8a0919091d9768ebe36f2af8a8a037d2c3cc0b
Enrico Gueli <enrico@quince.nl>
2015-07-01 11:17:41 +0200

    added projects

---
+++ WithoutTestOptions/Project/lib/src/test/java/test/lib/AndroidMockTest.java
@@ -0,0 +1,15 @@
+package test.lib;
+
+import android.util.Log;
+
+import org.junit.Test;
+
+/**
+ * Created by enrico on 01/07/15.
+ */
+public class AndroidMockTest {
+    @Test
+    public void testAndroidMocks() {
+        Log.v("test", "test");
+    }
+}
